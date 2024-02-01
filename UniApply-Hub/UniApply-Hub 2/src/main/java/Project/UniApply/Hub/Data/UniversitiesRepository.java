ec04770f1ad20de09929e48b47b9e216d41b0fbf
BlackRose1311 <mr.powers1311@gmail.com>
2024-02-01 14:11:33 -0600

    last update on university queery

--- UniApply-Hub/UniApply-Hub 2/src/main/java/Project/UniApply/Hub/Data/UniversitiesRepository.java
+++ UniApply-Hub/UniApply-Hub 2/src/main/java/Project/UniApply/Hub/Data/UniversitiesRepository.java
@@ -19,6 +19,6 @@ public interface UniversitiesRepository extends CrudRepository<Universities, Int
 //    @Query("SELECT * FROM universitiesWHERE id NOT IN (SELECT university_id FROM stu_uni  WHERE student_id =?1)")
 //    Universities findUniversitiesNotApplied(int studentId);
 
-    @Query(value = "SELECT * FROM Universities  WHERE Universities.id NOT IN (SELECT stu_uni.university_id FROM stu_uni  WHERE stu_uni.student_id =?1)", nativeQuery = true)
+    @Query(value = " SELECT * FROM Universities  WHERE Universities.id NOT IN (SELECT stuform_uni.university_id FROM stuform_uni  left join studentform on  studentform.id=stuform_uni .studentForm_id WHERE studentform.student_id =?1)", nativeQuery = true)
     List<Universities> findUniversitiesNotApplied(int studentId);
 }
